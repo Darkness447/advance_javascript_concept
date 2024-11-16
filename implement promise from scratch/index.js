54713d13eb119a81e98adf8059b49b50ff51b311
Darkness <agrawaldeepesh546@gmail.com>
2024-11-16 15:11:26 +0530

    refactor MyPromise implementation and add static methods: resolve, reject, all, allSettled, race, and any

--- implement promise from scratch/index.js
+++ implement promise from scratch/index.js
@@ -1,85 +1,220 @@
-// build promise classs
+// build promise from scrath
 
-// this is enum
+
 const STATE = {
-    PENDING: "pending",
-    SUCCESS: "success",
-    REJECTED: "rejected"
+    FULLFILLED: "fullfilled",
+    REJECTED: "rejected",
+    PENDING: "pending"
 }
 
-
 class MyPromise {
-    constructor(executeFunction) {
-        this.val = null;
-        this.state = STATE.PENDING
-        this.successCallbacks = []
-        this.failedCallbacks = []
 
+    #thenCbs = []
+    #catchCbs = []
+    #state = STATE.PENDING
+    #value
+    #onSuccessBind = this.#onSuccess.bind(this)
+    #onFailBind = this.#onFail.bind(this)
+
+    constructor(cb) {
         try {
-            executeFunction(
-                val => this.resolve(val),
-                val => this.reject(val)
-            )
-        } catch (error) {
-            this.reject(error)
+            cb(this.#onSuccessBind, this.#onFailBind)
+        } catch (e) {
+            this.#onFail(e);
         }
     }
 
-    resolve(val) {
-        this.val = val;
-        this.state = STATE.SUCCESS
-        this.successCallbacks.forEach(cb => cb());
+    #runCallBacks() {
+        if (this.#state === STATE.FULLFILLED) {
+            this.#thenCbs.forEach(callback => {
+                callback(this.#value)
+            })
+
+            this.#thenCbs = []
+        }
+
+        if (this.#state === STATE.REJECTED) {
+            this.#catchCbs.forEach(callback => {
+                callback(this.#value)
+            })
+
+            this.#catchCbs = []
+        }
     }
 
-    reject(val) {
-        this.val = val;
-        this.state = STATE.REJECTED
-        this.failedCallbacks.forEach(cb => cb());
+    #onSuccess(value) {
+        queueMicrotask(() => {
+            if (this.#state === STATE.PENDING) {
 
-    }
-
-    then(onResolve, onReject) {
-        return new MyPromise((resolve, reject) => {
-
-            const successCaller = () => {
-                if (!onResolve) return resolve(this.val)
-
-                try {
-                    let val = onResolve(this.val);
-                    resolve(onResolve)
-                } catch (error) {
-                    reject(error)
+                if (value instanceof MyPromise) {
+                    value.then(this.#onSuccessBind, this.#onFailBind)
+                    return
                 }
-            }
-
-            const failedCaller = () => {
-                if (!onReject) return reject(this.val)
-
-                try {
-                    let val = onReject(this.val);
-                    resolve(onResolve)
-                } catch (error) {
-                    reject(error)
-                }
-            }
-            switch (this.state) {
-                case STATE.PENDING:
-                    this.successCallbacks.push(successCaller)
-                    this.failedCallbacks.push(failedCaller)
-                    break;
-                case STATE.SUCCESS:
-                    successCaller()
-                    break;
-                case STATE.REJECTED:
-                    failedCaller();
-                    break;
-                default:
-                    throw new Error("State not defined")
+                this.#value = value
+                this.#state = STATE.FULLFILLED
+                this.#runCallBacks();
             }
         })
     }
 
-    catch(onReject) {
-        return this.then(null, onReject)
+    #onFail(value) {
+
+        queueMicrotask(() => {
+            if (this.#state === STATE.PENDING) {
+
+                if (value instanceof MyPromise) {
+                    value.then(this.#onSuccessBind, this.#onFailBind)
+                    return
+                }
+
+                if (this.#catchCbs.length === 0) {
+                    throw new UncaugthPromiseError("value")
+                }
+
+                this.#value = value
+                this.#state = STATE.REJECTED
+                this.#runCallBacks();
+            }
+        })
+    }
+
+
+    then(thenCb, catchCb) {
+
+        return new MyPromise((resolve, reject) => {
+            this.#thenCbs.push(result => {
+                if (thenCb === null) {
+                    resolve(result)
+                    return;
+                }
+
+                try {
+                    resolve(thenCb(result))
+                } catch (e) {
+                    reject(e)
+                }
+            })
+
+            this.#catchCbs.push(result => {
+                if (catchCb === null) {
+                    reject(result)
+                    return;
+                }
+
+                try {
+                    resolve(catchCb(result))
+                } catch (e) {
+                    reject(e)
+                }
+            })
+
+            this.#runCallBacks()
+        })
+    }
+
+    catch(cb) {
+        return this.then(undefined, cb)
+    }
+
+    finally(cb) {
+        return this.then(result => {
+            cb();
+            return result
+        }, result => {
+            cb();
+            throw result
+        })
+    }
+
+
+    static resolve(value) {
+        return new Promise((resolve) => {
+            resolve(value)
+        })
+    }
+
+
+    static reject(value) {
+        return new Promise((_, reject) => {
+            reject(value)
+        })
+    }
+
+    static all(promises) {
+        const results = []
+        let completedPromises = 0
+        return new MyPromise((resolve, reject) => {
+            for (let i = 0; i < promises.length; i++) {
+                const promise = promises[i]
+                promise
+                    .then(value => {
+                        completedPromises++
+                        results[i] = value
+                        if (completedPromises === promises.length) {
+                            resolve(results)
+                        }
+                    })
+                    .catch(reject)
+            }
+        })
+    }
+
+    static allSettled(promises) {
+        const results = []
+        let completedPromises = 0
+        return new MyPromise(resolve => {
+            for (let i = 0; i < promises.length; i++) {
+                const promise = promises[i]
+                promise
+                    .then(value => {
+                        results[i] = { status: STATE.FULLFILLED, value }
+                    })
+                    .catch(reason => {
+                        results[i] = { status: STATE.REJECTED, reason }
+                    })
+                    .finally(() => {
+                        completedPromises++
+                        if (completedPromises === promises.length) {
+                            resolve(results)
+                        }
+                    })
+            }
+        })
+    }
+
+    static race(promises) {
+        return new MyPromise((resolve, reject) => {
+            promises.forEach(promise => {
+                promise.then(resolve).catch(reject)
+            })
+        })
+    }
+
+    static any(promises) {
+        const errors = []
+        let rejectedPromises = 0
+        return new MyPromise((resolve, reject) => {
+            for (let i = 0; i < promises.length; i++) {
+                const promise = promises[i]
+                promise.then(resolve).catch(value => {
+                    rejectedPromises++
+                    errors[i] = value
+                    if (rejectedPromises === promises.length) {
+                        reject(new AggregateError(errors, "All promises were rejected"))
+                    }
+                })
+            }
+        })
+    }
+
+}
+
+class UncaugthPromiseError extends Error {
+    constructor(error) {
+        super(error)
+        this.stack = `(in promise) ${error.stack}`
     }
 }
+
+module.exports = MyPromise
+
